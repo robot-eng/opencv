938cb948e36aed3789590d32efb4aa3ccb7edb27
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2021-09-04 16:36:46 +0700

    Update face_eye.py

--- face_eye/face_eye.py
+++ face_eye/face_eye.py
@@ -1,12 +1,8 @@
 import numpy as np
 import cv2
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_eye.xml')
 
